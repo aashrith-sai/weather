1c74efded7ccd2bbe6262761d52778da4a8c16f2
aashrith <taashrith@gmail.com>
2020-05-28 16:18:51 -0500

    a

--- weather.py
+++ weather.py
@@ -7,41 +7,12 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
+    
     connection = mysql.connector.connect(host='localhost',
                                          database='weather',
                                          user='root',
                                          password='password')
-
-    mySql_Create_Table_Query = """CREATE TABLE Current (
-                             lat DOUBLE,
-                             lon DOUBLE,
-                             alt_m DOUBLE,
-                             alt_f DOUBLE,
-                             wx_desc VARCHAR(30),
-                             wx_code INT,
-                             wx_icon VARCHAR(300),
-                             temp_c DOUBLE,
-                             temp_f DOUBLE,
-                             feelslike_c DOUBLE,
-                             feelslike_f DOUBLE,
-                             windspd_mph INT,
-                             windspd_kmh INT,
-                             windspd_kts INT,
-                             windspd_ms DOUBLE,
-                             winddir_deg INT,
-                             winddir_compass VARCHAR(20),
-                             cloudtotal_pct INT,
-                             humid_pct INT,
-                             dewpoint_c DOUBLE,
-                             vis_km DOUBLE,
-                             vis_mi DOUBLE,
-                             slp_mb INT,
-                             slp_in DOUBLE); """
-    connection = mysql.connector.connect(host='localhost',
-                                         database='weather',
-                                         user='root',
-                                         password='password')
-    mySql_insert_query = """INSERT INTO Laptop (lat,
+    mySql_insert_query = """INSERT INTO Current (lat,
                              lon,
                              alt_m,
                              alt_f,
