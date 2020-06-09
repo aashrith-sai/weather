4a46dbcae74b8fe443c059c5851aed4b4ff8210c
aashrith <taashrith@gmail.com>
2020-06-09 12:27:03 -0500

    a

--- weather.py
+++ weather.py
@@ -2,17 +2,15 @@
 
 
 import requests
+import pymysql
 import pgeocode
-import mysql.connector
-from mysql.connector import Error
+import json
 
-try:
-    
-    connection = mysql.connector.connect(host='public-data-1.c9omh8sjvlrw.us-east-2.rds.amazonaws.com',
+connection = pymysql.connect(host='public-data-1.c9omh8sjvlrw.us-east-2.rds.amazonaws.com',
                                          database='weather',
                                          user='admin',
                                          password="9!srR}G'PgD+R%cD")
-    query1=""" CREATE TABLE IF NOT EXISTS Current(zip VARCHAR(8),
+query1=""" CREATE TABLE IF NOT EXISTS Current(zip VARCHAR(8),
                              lat DOUBLE,
                              lon DOUBLE,
                              alt_m DOUBLE,
@@ -37,31 +35,33 @@ try:
                              vis_mi DOUBLE,
                              slp_mb INT,
                              slp_in DOUBLE);"""
-    cursor = connection.cursor()
-    cursor.execute(query1)
-    connection.commit()
+cursor = connection.cursor()
+cursor.execute(query1)
+connection.commit()
 
-    mycursor = connection.cursor()
-    mycursor.execute("SELECT zipcode FROM zip")
-    zip=mycursor.fetchall()
-    print(zip[0][0])
+mycursor = connection.cursor()
+mycursor.execute("SELECT zipcode FROM zip")
+zip=mycursor.fetchall()
+    #print(zip[0][0])
 
-    for i in range(len(zip)):
+for i in range(len(zip)):
+    from uszipcode import SearchEngine
+    search = SearchEngine(simple_zipcode=True) # set simple_zipcode=False to use rich info databas
+    zipcode = search.by_zipcode(str(zip[i][0]))
+        #nomi = pgeocode.Nominatim('us')
+        #loc=nomi.query_postal_code(str(zip[i][0]))
+    URL = "http://api.weatherunlocked.com/api/current/"
+    appID='f134de62'
+    appKey='37ed7f8fb3d413880a6659c6240272d6'
+    PARAMS = { 'app_id': appID, 'app_key': appKey} 
+    r = requests.get(url = URL+str(zipcode.lat)+','+str(zipcode.lng), params = PARAMS) 
+    data = r.json()
+    URLf='http://api.weatherunlocked.com/api/forecast/'
+    rf = requests.get(url = URLf+str(zipcode.lat)+','+str(zipcode.lng), params = PARAMS) 
+    dataf=rf.json()
+    c,f= data,dataf
 
-        nomi = pgeocode.Nominatim('us')
-        loc=nomi.query_postal_code(str(zip[i][0]))
-        URL = "http://api.weatherunlocked.com/api/current/"
-        appID='f134de62'
-        appKey='37ed7f8fb3d413880a6659c6240272d6'
-        PARAMS = { 'app_id': appID, 'app_key': appKey} 
-        r = requests.get(url = URL+str(loc.latitude)+','+str(loc.longitude), params = PARAMS) 
-        data = r.json()
-        URLf='http://api.weatherunlocked.com/api/forecast/'
-        rf = requests.get(url = URLf+str(loc.latitude)+','+str(loc.longitude), params = PARAMS) 
-        dataf=rf.json()
-        c,f= data,dataf
-
-        mySql_insert_query = """INSERT INTO Current (zip,
+    mySql_insert_query = """INSERT INTO Current (zip,
                              lat,
                              lon,
                              alt_m,
@@ -86,7 +86,7 @@ try:
                              vis_mi,
                              slp_mb,
                              slp_in) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s); """
-        val=(zip[i][0],c['lat'],c['lon'],
+    val=(zip[i][0],c['lat'],c['lon'],
                              c['alt_m'],
                              c['alt_ft'],
                              c['wx_desc'],
@@ -109,10 +109,10 @@ try:
                              c['vis_mi'],
                              c['slp_mb'],
                              c['slp_in'])
-        cursor.execute(mySql_insert_query,val)
-        connection.commit()
+    cursor.execute(mySql_insert_query,val)
+    connection.commit()
 
-    query2=""" CREATE TABLE IF NOT EXISTS Forecast(zip VARCHAR(8),date VARCHAR(20),
+query2=""" CREATE TABLE IF NOT EXISTS Forecast(zip VARCHAR(8),date VARCHAR(20),
                             sunrise_time VARCHAR(10),
                             sunset_time VARCHAR(10),
                             moonrise_time VARCHAR(10),
@@ -142,93 +142,89 @@ try:
                             slp_max_mb INT,
                             slp_min_in DOUBLE,
                             slp_min_mb INT);"""
-    query3="""DELETE FROM Forecast;"""
+query3="""DELETE FROM Forecast;"""
 
-    cursor.execute(query2)
-    connection.commit()
-    cursor.execute(query3)
-    connection.commit()
-    for j in range(len(zip)):
-        nomi = pgeocode.Nominatim('us')
-        loc=nomi.query_postal_code(str(zip[j][0]))
-        URL = "http://api.weatherunlocked.com/api/current/"
-        appID='f134de62'
-        appKey='37ed7f8fb3d413880a6659c6240272d6'
-        PARAMS = { 'app_id': appID, 'app_key': appKey} 
-        r = requests.get(url = URL+str(loc.latitude)+','+str(loc.longitude), params = PARAMS) 
-        data = r.json()
-        URLf='http://api.weatherunlocked.com/api/forecast/'
-        rf = requests.get(url = URLf+str(loc.latitude)+','+str(loc.longitude), params = PARAMS) 
-        dataf=rf.json()
-        c,f= data,dataf
-        for i in range(7):
-            query4="""INSERT INTO Forecast (zip,date,
-            sunrise_time,
-            sunset_time,
-            moonrise_time,
-            moonset_time,
-            temp_max_c,
-            temp_max_f,
-            temp_min_c,
-            temp_min_f,
-            precip_total_mm,
-            precip_total_in,
-            rain_total_mm,
-            rain_total_in,
-            snow_total_mm,
-            snow_total_in,
-            prob_precip_pct,
-            humid_max_pct,
-            humid_min_pct,
-            windspd_max_mph,
-            windspd_max_kmh,
-            windspd_max_kts,
-            windspd_max_ms,
-            windgst_max_mph,
-            windgst_max_kmh,
-            windgst_max_kts,
-            windgst_max_ms,
-            slp_max_in,
-            slp_max_mb,
-            slp_min_in,
-            slp_min_mb ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
-            val=(zip[j][0],f['Days'][i]['date'],
-            f['Days'][i]['sunrise_time'],
-            f['Days'][i]['sunset_time'],
-            f['Days'][i]['moonrise_time'],
-            f['Days'][i]['moonset_time'],
-            f['Days'][i]['temp_max_c'],
-            f['Days'][i]['temp_max_f'],
-            f['Days'][i]['temp_min_c'],
-            f['Days'][i]['temp_min_f'],
-            f['Days'][i]['precip_total_mm'],
-            f['Days'][i]['precip_total_in'],
-            f['Days'][i]['rain_total_mm'],
-            f['Days'][i]['rain_total_in'],
-            f['Days'][i]['snow_total_mm'],
-            f['Days'][i]['snow_total_in'],
-            f['Days'][i]['prob_precip_pct'],
-            f['Days'][i]['humid_max_pct'],
-            f['Days'][i]['humid_min_pct'],
-            f['Days'][i]['windspd_max_mph'],
-            f['Days'][i]['windspd_max_kmh'],
-            f['Days'][i]['windspd_max_kts'],
-            f['Days'][i]['windspd_max_ms'],
-            f['Days'][i]['windgst_max_mph'],
-            f['Days'][i]['windgst_max_kmh'],
-            f['Days'][i]['windgst_max_kts'],
-            f['Days'][i]['windgst_max_ms'],
-            f['Days'][i]['slp_max_in'],
-            f['Days'][i]['slp_max_mb'],
-            f['Days'][i]['slp_min_in'],
-            f['Days'][i]['slp_min_mb'] )
-            cursor.execute(query4,val)
-            connection.commit()
-    print(cursor.rowcount, "Records inserted successfully into tables")
-    cursor.close()
-
-except mysql.connector.Error as error:
-    print("Failed to create table in MySQL: {}".format(error))
-
-
-
+cursor.execute(query2)
+connection.commit()
+cursor.execute(query3)
+connection.commit()
+for j in range(len(zip)):
+    search = SearchEngine(simple_zipcode=True) # set simple_zipcode=False to use rich info databas
+    zipcode = search.by_zipcode(str(zip[j][0]))
+    #nomi = pgeocode.Nominatim('us')
+    #loc=nomi.query_postal_code(str(zip[j][0]))
+    URL = "http://api.weatherunlocked.com/api/current/"
+    appID='f134de62'
+    appKey='37ed7f8fb3d413880a6659c6240272d6'
+    PARAMS = { 'app_id': appID, 'app_key': appKey} 
+    r = requests.get(url = URL+str(zipcode.lat)+','+str(zipcode.lng), params = PARAMS) 
+    data = r.json()
+    URLf='http://api.weatherunlocked.com/api/forecast/'
+    rf = requests.get(url = URLf+str(zipcode.lat)+','+str(zipcode.lng), params = PARAMS) 
+    dataf=rf.json()
+    c,f= data,dataf
+    for i in range(7):
+        query4="""INSERT INTO Forecast (zip,date,
+                sunrise_time,
+                sunset_time,
+                moonrise_time,
+                moonset_time,
+                temp_max_c,
+                temp_max_f,
+                temp_min_c,
+                temp_min_f,
+                precip_total_mm,
+                precip_total_in,
+                rain_total_mm,
+                rain_total_in,
+                snow_total_mm,
+                snow_total_in,
+                prob_precip_pct,
+                humid_max_pct,
+                humid_min_pct,
+                windspd_max_mph,
+                windspd_max_kmh,
+                windspd_max_kts,
+                windspd_max_ms,
+                windgst_max_mph,
+                windgst_max_kmh,
+                windgst_max_kts,
+                windgst_max_ms,
+                slp_max_in,
+                slp_max_mb,
+                slp_min_in,
+                slp_min_mb ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
+        val=(zip[j][0],f['Days'][i]['date'],
+                f['Days'][i]['sunrise_time'],
+                f['Days'][i]['sunset_time'],
+                f['Days'][i]['moonrise_time'],
+                f['Days'][i]['moonset_time'],
+                f['Days'][i]['temp_max_c'],
+                f['Days'][i]['temp_max_f'],
+                f['Days'][i]['temp_min_c'],
+                f['Days'][i]['temp_min_f'],
+                f['Days'][i]['precip_total_mm'],
+                f['Days'][i]['precip_total_in'],
+                f['Days'][i]['rain_total_mm'],
+                f['Days'][i]['rain_total_in'],
+                f['Days'][i]['snow_total_mm'],
+                f['Days'][i]['snow_total_in'],
+                f['Days'][i]['prob_precip_pct'],
+                f['Days'][i]['humid_max_pct'],
+                f['Days'][i]['humid_min_pct'],
+                f['Days'][i]['windspd_max_mph'],
+                f['Days'][i]['windspd_max_kmh'],
+                f['Days'][i]['windspd_max_kts'],
+                f['Days'][i]['windspd_max_ms'],
+                f['Days'][i]['windgst_max_mph'],
+                f['Days'][i]['windgst_max_kmh'],
+                f['Days'][i]['windgst_max_kts'],
+                f['Days'][i]['windgst_max_ms'],
+                f['Days'][i]['slp_max_in'],
+                f['Days'][i]['slp_max_mb'],
+                f['Days'][i]['slp_min_in'],
+                f['Days'][i]['slp_min_mb'] )
+        cursor.execute(query4,val)
+        connection.commit()
+print(cursor.rowcount, "Records inserted successfully into tables")
+cursor.close()
